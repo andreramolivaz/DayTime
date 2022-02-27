c6548f16ff29cba368cd44e55403e4f89a6a61ca
andreramolivaz <92636448+andreramolivaz@users.noreply.github.com>
2022-02-27 17:44:17 +0100

    Add files via upload

---
+++ DayTimePersonnettaz.java
@@ -0,0 +1,49 @@
+package daytime.personnettaz;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketTimeoutException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author personal
+ */
+public class DayTimePersonnettaz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        String serverAddress;
+        serverAddress="casaroby.dlinkddns.com";
+        int serverPort=13;
+        byte buffer[] = new byte[512];
+        
+        while(true){
+            DatagramPacket outPacket = new DatagramPacket("".getBytes(), 0, InetAddress.getByName(serverAddress), serverPort);
+            DatagramPacket inPacket = new DatagramPacket(buffer, buffer.length);
+            DatagramSocket dSocket = new DatagramSocket();
+
+            dSocket.send(outPacket);
+            
+            dSocket.setSoTimeout(4000);
+            
+            try{
+                dSocket.receive(inPacket);
+                System.out.println("Pacchetto ricevuto: "+new String(buffer,0,inPacket.getLength()));
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(DayTimePersonnettaz.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            catch(SocketTimeoutException ex){
+                System.out.println("Il server non ha rispostonto entro i 4 secondi stabiliti.");
+            }
+        }
+    }
+}
